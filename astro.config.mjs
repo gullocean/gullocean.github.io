e7a912c620b5ac4231be1b237be4ed685fd30ab0
Gull Ocean <gullocean@gmail.com>
2024-12-12 03:22:39 -0400

    minor correction

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://gullocean.github.io',
-  base: '/portfolio',
+  base: '/',
   integrations: [tailwind()],
   output: 'static',
   build: {
