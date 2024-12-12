34cb07c1279381fd7e980c45e0cca1001dbf9f5a
Gull Ocean <gullocean@gmail.com>
2024-12-12 03:15:29 -0400

    correction of site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://yourusername.github.io',
+  site: 'https://gullocean.github.io',
   base: '/portfolio',
   integrations: [tailwind()],
   output: 'static'
